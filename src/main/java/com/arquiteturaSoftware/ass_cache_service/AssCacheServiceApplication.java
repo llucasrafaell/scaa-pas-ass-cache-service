652ae00b5b5ffb4b8ce5d3bdf4462056c4fb33e6
Lucas <lucasmarthins1@gmail.com>
2024-11-18 20:45:20 -0300

    Adicionando arquivos iniciais

---
+++ src/main/java/com/arquiteturaSoftware/ass_cache_service/AssCacheServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arquiteturaSoftware.ass_cache_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssCacheServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssCacheServiceApplication.class, args);
+	}
+
+}
